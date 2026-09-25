a78fbb0b0ab05a9d88ed5889e476b0670599eafe
VINITH S SHETTY <vinuvinith0007@gmail.com>
2026-09-25 17:06:02 +0530

    Initialize Capacitor for Android app wrapping

---
+++ android/app/src/main/java/com/mypet9/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.mypet9.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mypet9.app',
+  appName: 'MyPet9',
+  webDir: 'dist'
+};
+
+export default config;
